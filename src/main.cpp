17b16685d336d090d3d94f64ba6ae384073f03db
dragon <dima_rodin@yahoo.com>
2024-09-11 22:11:15 +0300

    DESIGN: #1: removed GUI
    * removed GUI
    * added Logger class
    * added some preparation to using Logger
    
    Issue: Server #18

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,49 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QCoreApplication>
+#include <QCommandLineParser>
+
 #include "../include/BackEnd.h"
+#include "../include/Logger.h"
 #include "../include/TcpDumpManager.h"
 
 int main( int argc, char *argv[] )
 {
     QCoreApplication::setAttribute( Qt::AA_EnableHighDpiScaling );
-    QGuiApplication app( argc, argv );
+    QCoreApplication app(argc, argv);
 
-    qmlRegisterType< BackEnd >( "io.qt.BackEnd", 1, 0, "BackEnd" );
-    qmlRegisterType< TcpDumpManager >( "io.qt.TcpDumpManager", 1, 0, "TcpDumpManager" );
-    QQmlApplicationEngine engine;
+    // Create the command-line parser
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Test QCommandLineParser");
+    parser.addHelpOption(); // Adds --help option by default
 
-    const QUrl url( QStringLiteral( "qrc:/qml/main.qml" ) );
-    QObject::connect( &engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url]( QObject *obj, const QUrl &objUrl ) {
-        if ( !obj && url == objUrl )
-            QCoreApplication::exit( -1 );
-    }, Qt::QueuedConnection );
-    engine.load( url );
+    // Define options (command-line flags)
+    QCommandLineOption noLogOption(QStringList() << "n" << "no-log", "Disable logging.");
+    QCommandLineOption consoleLogOption(QStringList() << "c" << "console-log", "Log to console.");
+    QCommandLineOption logFileOption(QStringList() << "f" << "file-log", "Log to file.");
+
+    // Add options to the parser
+    parser.addOption(noLogOption);
+    parser.addOption(consoleLogOption);
+    parser.addOption(logFileOption);
+
+    // Process the actual command-line arguments given by the user
+    parser.process(app);
+
+    std::shared_ptr< ILogger > logger;
+
+    // Handle the command-line options
+    if (parser.isSet(noLogOption)) {
+        qDebug() << "Logging is disabled.";
+    } else if (parser.isSet(consoleLogOption)) {
+        qDebug() << "Logging to console.";
+        logger = std::make_shared< ConsoleLogger >();
+    } else if (parser.isSet(logFileOption)) {
+        qDebug() << "Logging to file (default).";
+        logger = std::make_shared< FileLogger > ( "/var/log/myapp/application.log" );
+    } else {
+        qDebug() << "No specific logging option set. Logging is disabled.";
+    }
+
+    BackEnd backEnd( logger );
 
     return app.exec();
 }
